fa8ca07682397030bf423ecabbf4b623b8bc414a
Ujjval Sinha <ujjvalsinha@Ujjvals-MacBook-Air.local>
2020-01-30 03:00:23 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:accurateweather/Home.dart';
+import 'package:flutter/material.dart';
+
+main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
